83c8adaff585c96fd6ce8b2768242393c7db9c53
懒羊佳 <lanyangjia@outlook.com>
2020-05-28 15:02:38 +0800

    admin add blog

--- itassets/blog/urls.py
+++ itassets/blog/urls.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
 
-from django.urls import path
+from django.urls import path, include
 
 
-app_name = 'blog'
+# app_name = 'blog'
+# urlpatterns = [
+#     path('', 'blog.views.index', name='index'),
+#     path('', 'blog.views.post', name='post'),
+# ]
 urlpatterns = [
-    path('', 'blog.views.index', name='index'),
-    path('', 'blog.views.post', name='post'),
+    path('admin/', admin.site.urls),
+    path('', views.hello),
+    # path('ueditor/', include('DjangoUeditor.urls')),
 ]
--- itassets/itassets/settings.py
+++ itassets/itassets/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    # 'DjangoUeditor',
 ]
 
 MIDDLEWARE = [
@@ -105,9 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
@@ -120,3 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
